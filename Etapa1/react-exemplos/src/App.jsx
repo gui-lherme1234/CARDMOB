508f54472817e116563c5567ccd4f74964df3f56
gui-lherme1234 <donizetti.g@aluno.ifsp.edu.br>
2025-03-11 09:55:05 -0300

    11/03/2025

--- Etapa1/react-exemplos/src/App.jsx
+++ Etapa1/react-exemplos/src/App.jsx
@@ -1,46 +1,48 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import Counter from './components/Counter';
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [photos, setPhotos] = useState([]);
 
-  // function updateCount() {
-  //   setCount(count+1);
-  // }
-
-  // arraw function 
-
-  const updateCount = () => {
-    // outros comandos
-    return count + 1;
-  }
-
-  const updateCount1 = () => count + 1; // return é implicito
-
-  const dados = {
-    "nome": "fulano",
-    "atualiza": (novo_nome) => `Nome nome é ${novo_nome}`,
-    "endereco": {
-      "rua": "xyz",
-      "numero": "111",
-      "complementos": ["casa", "na esquina do supermercado ABC"]
+  const fetchPhotos = async () => {
+    try {
+      const url = 'https://jsonplaceholder.typicode.com/albums/1/photos';
+      const response = await fetch(url);
+      if (response.status === 200) {
+        const data = await response.json();
+        const updatedData = data.map((photo) => ({
+          ...photo,
+          thumbnailUrl: `https://picsum.photos/300?random=${photo.id}`
+        }));
+        setPhotos(updatedData);
+      }
+    } catch (error) {
+      console.error('Erro na busca', error);
     }
+  };
 
-  }; // é um objeto JS
-  dados.atualiza("gerson");
-  dados.endereco.complementos[1]; // acessando a referência do endereço
-
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
 
   return (
-    <>
-      <Counter title="Contando..." />
-      <Counter initial="100" />
-    </>
-  )
+    <div>
+      <h1>Meu álbum</h1>
+      {photos.map((photo) => {
+        return (
+          <div key={photo.id}>
+            <img src={photo.thumbnailUrl} alt={photo.title} />
+            <p>ID #{photo.id}</p>
+            <h2>{photo.title}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
