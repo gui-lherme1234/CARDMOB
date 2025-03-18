81fc0a797f01943b5c837a764638c15c65c72c6d
gui-lherme1234 <donizetti.g@aluno.ifsp.edu.br>
2025-03-18 09:14:50 -0300

    feat: add Album component

--- Etapa1/react-exemplos/src/App.jsx
+++ Etapa1/react-exemplos/src/App.jsx
@@ -3,22 +3,25 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Counter from "./components/Counter";
+import Photo from "./components/photo";
+import Album from "./components/album";
 
 function App() {
   const [count, setCount] = useState(0);
   const [photos, setPhotos] = useState([]);
+  const [albumId, setAlbumId] = useState(1);
 
   const fetchPhotos = async () => {
     try {
-      const url = 'https://jsonplaceholder.typicode.com/albums/1/photos';
+      const url = `https://jsonplaceholder.typicode.com/albums/${albumId}/photos`;
       const response = await fetch(url);
       if (response.status === 200) {
         const data = await response.json();
-        const updatedData = data.map((photo) => ({
+        const updatedPhotos = data.map((photo) => ({
           ...photo,
-          thumbnailUrl: `https://picsum.photos/300?random=${photo.id}`
+          thumbnailUrl: `https://picsum.photos/300?random=${photo.id}`,
         }));
-        setPhotos(updatedData);
+        setPhotos(updatedPhotos);
       }
     } catch (error) {
       console.error('Erro na busca', error);
@@ -27,21 +30,26 @@ function App() {
 
   useEffect(() => {
     fetchPhotos();
-  }, []);
+  }, [albumId]); // Agora, sempre que o `albumId` mudar, a função `fetchPhotos` será chamada novamente.
 
   return (
-    <div>
-      <h1>Meu álbum</h1>
-      {photos.map((photo) => {
-        return (
-          <div key={photo.id}>
-            <img src={photo.thumbnailUrl} alt={photo.title} />
-            <p>ID #{photo.id}</p>
-            <h2>{photo.title}</h2>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <Counter title="Contando..."/>
+      <Counter initial="100"/>
+      {/* <article>
+        <h1>Meu álbum</h1>
+        {photos.map((photo) => (
+          <Photo key={photo.id} photo={photo} />
+        ))}
+      </article> */}
+      <div>
+        <button onClick={() => setAlbumId(1)}>Album #1</button>
+        <button onClick={() => setAlbumId(2)}>Album #2</button>
+        <button onClick={() => setAlbumId(3)}>Album #3</button>
+        <button onClick={() => setAlbumId(4)}>Album #4</button>
+      </div>
+      <Album albumId={albumId} />
+    </>
   );
 }
 
