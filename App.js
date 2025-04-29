bd9613950c71b065b7def65cbf224b7fe5aca51e
gui-lherme1234 <donizetti.g@aluno.ifsp.edu.br>
2025-04-29 08:54:03 -0300

    Exemplo: contador + imagem

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCounter = () => {
+    setCount(count - 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Olá App React Native - Atualiza!</Text>
+      <Image
+        source={{ uri: "https://picsum.photos/200"}}
+        style={{ width: 200, height: 200 }}
+      />
+      <Text style={styles.text}>Contador: {count}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Incrementar" onPress={incrementCounter} />
+        <Button title="Decrementar" onPress={decrementCounter} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +36,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    gap: 10,
+    marginTop: 20,
   },
 });
