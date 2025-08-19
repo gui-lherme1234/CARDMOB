e3d750fce9b546ab31abe38c3336240854b44d1e
gui-lherme1234 <donizetti.g@aluno.ifsp.edu.br>
2025-08-19 08:47:27 -0300

    Inclui novas telas: registro e login

--- Etapa 3/loja-react-app/src/navigation/RootNavigator.tsx
+++ Etapa 3/loja-react-app/src/navigation/RootNavigator.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { RootStackParamList, TabParamList } from './types';
-// Telas do app
-import HomeScreen from '../screens/HomeScreen';
-// importar, detailsScreen, settingsSreen
+
+// Telas do app.
+import HomeScreen from "../screens/HomeScreen";
+// importar depois que implementar: DetailsScreen, SettingsScreen
+import RegisterScreen from "../screens/RegisterScreen";
+import LoginScreen from "../screens/LoginScreen";
 
 const AppStack = createNativeStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator<TabParamList>();
 
 function TabNavigator() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={HomeScreen} />
-    </Tab.Navigator>
-  );
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen}/>
+            <Tab.Screen name="Settings" component={HomeScreen} />
+            <Tab.Screen name="Register" component={RegisterScreen} />
+        </Tab.Navigator>
+    );
 }
+
 function AppNavigator() {
   return (
     <AppStack.Navigator>
@@ -26,19 +31,24 @@ function AppNavigator() {
         component={TabNavigator}
         options={{ headerShown: false }}
       />
-
       <AppStack.Screen
         name="Details"
         component={HomeScreen}
         options={{ title: 'Detalhes' }}
       />
+      <AppStack.Screen 
+        name="Login"
+        component={LoginScreen}
+        options={{ title: "Acessar" }}
+      />
     </AppStack.Navigator>
   );
 }
+
 export default function RootNavigator() {
   return (
     <NavigationContainer>
       <AppNavigator />
     </NavigationContainer>
   );
-}
+};
--- Etapa 3/loja-react-app/src/screens/HomeScreen.tsx
+++ Etapa 3/loja-react-app/src/screens/HomeScreen.tsx
@@ -1,23 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
+// EXCLUIR: import { useNavigation } from "@react-navigation/native";
+import { useTheme } from '../contexts/ThemeContext'; // Nova
 
-import { useTheme } from '../contexts/ThemeContext'; // Assumindo que esse hook está implementado corretamente
-
-function HomeScreen() {
+function HomeScreen({ navigation }: any) { // INCLUIR {navigation}: any
+    // EXCLUIR const navigation = useNavigation();
     const { theme, toggleTheme } = useTheme();
-
     return (
-        <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
-            <Text style={{ color: theme.colors.text, marginBottom: theme.spacing(1) }}>Home Screen</Text>
-            <Button
-                title="Alternar tema"
-                color={theme.colors.primary}
-                onPress={toggleTheme}
-            />
+        <View style={[styles.container, {backgroundColor: theme.colors.background}]}>
+            <Text style={{ color: theme.colors.text, marginBottom: theme.spacing(1) }}>
+                Home Screen
+            </Text>
+            <Button title="Alternar Tema" color={theme.colors.primary} onPress={toggleTheme}/>
+            <Button title="Ir para Detalhes" onPress={ () => navigation.navigate('Details')} />
+            <Button title="Login" onPress={ () => navigation.navigate('Login')}/>
         </View>
     );
 }
-
 export default HomeScreen;
 
 const styles = StyleSheet.create({
