6989f8ef3d463b56900121fca47effc9ee7214f0
gui-lherme1234 <donizetti.g@aluno.ifsp.edu.br>
2025-11-04 08:00:03 -0300

    Concluir carrinho de compras

--- Etapa 3/loja-react-app/src/context/ShopContext.tsx
+++ Etapa 3/loja-react-app/src/context/ShopContext.tsx
@@ -1,37 +1,61 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 type ShopContextType = {
-  cartItems: any[];
-  addToCart: (item: any) => Promise<void>;
+    cartItems: any[];
+    addToCart: (item: any) => Promise<void>;
+    removeFromCart: (itemId: number) => Promise<void>;
+    getTotalPrice: () => number;
+    clearCart: () => void;
 };
 
-export const ShopContext = createContext<ShopContextType>(
-  {} as ShopContextType
-);
+export const ShopContext = createContext<ShopContextType>({} as ShopContextType);
 
-export const ShopProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
-  const [cartItems, setCartItems] = useState<any[]>([]);
+export const ShopProvider: React.FC<{ children: React.ReactNode}> = ({ children}) => {
+    const [cartItems, setCartItems] = useState<any[]>([]);
 
-  const addToCart = async (item: any, quantity: number = 1) => {
-    setCartItems((prevItems) => {
-      const existingIndex = prevItems.findIndex(
-        (cartItem) => cartItem.id === item.id
-      );
-      if (existingIndex >= 0) {
-        const updatedItems = [...prevItems];
-        if (updatedItems[existingIndex].quantity + quantity > 0) {
-          updatedItems[existingIndex].quantity += quantity;
-        }
-        return updatedItems;
-      } else {
-        return [...prevItems, { ...item, quantity }];
-      }
-    });
-  };
+    const addToCart = async (item: any, quantity: number = 1) => {
+        setCartItems(prevItems => {
+                const existingIndex = prevItems.findIndex(
+                    cartItem => cartItem.id === item.id
+                );
+                if (existingIndex >= 0) {
+                    const updatedItems = [...prevItems];
+                    if (updatedItems[existingIndex].quantity + quantity > 0) {
+                        updatedItems[existingIndex].quantity += quantity;
+                    }
+                    return updatedItems;
+                }
+                else {
+                    return [...prevItems, {...item, quantity}];
+                }
 
-  return <ShopContext value={{ cartItems, addToCart }}>{children}</ShopContext>;
-};
+            }
+        )
+    }
+
+    const removeFromCart = (itemId: number) => {
+        setCartItems((prevItems) =>  
+            prevItems.filter(item => item.id !== itemId)
+        );
+    }
+
+    const getTotalPrice = () => {
+        return cartItems.reduce(
+            (total, item) => total + item.price * item.quantity, 0
+        ).toFixed(2);
+    }
+
+    const clearCart = () => {
+        setCartItems([]);
+    }
+
+    return (
+        <ShopContext
+            value={ { cartItems, addToCart, removeFromCart, getTotalPrice, clearCart } }
+        >
+            {children}
+        </ShopContext>
+    );
+}
 
 export const useShop = () => useContext(ShopContext);
