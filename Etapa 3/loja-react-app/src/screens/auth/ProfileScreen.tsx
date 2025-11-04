c273bbfbe5f5d938804b359f2bb8ff9223930abb
gui-lherme1234 <donizetti.g@aluno.ifsp.edu.br>
2025-11-04 07:22:04 -0300

    Tela básica do perfil

--- Etapa 3/loja-react-app/src/screens/auth/ProfileScreen.tsx
+++ Etapa 3/loja-react-app/src/screens/auth/ProfileScreen.tsx
@@ -1,125 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  ActivityIndicator,
-  Image,
-  Button,
-  Platform,
-} from 'react-native';
-import { useTheme } from '../../context/ThemeContext';
-import { useAuth } from '../../context/AuthContext';
-import Constants from 'expo-constants';
+import React, { useEffect, useState } from "react"; // modificado
+import { View, Text, Button, StyleSheet, Image } from "react-native"; // modificado
 
-export const { apiUrl } = Constants.expoConfig?.extra || {};
+import { useTheme } from '../../contexts/ThemeContext';
+import { useAuth } from '../../contexts/AuthContext';
 
-export default function ProfileScreen({ navigation }: any) {
-  const { theme, toggleTheme } = useTheme();
-  const { logout } = useAuth();
+import { requestProfileById } from "../../services/profileService"; // novo 
 
-  const [user, setUser] = useState<any>(null);
-  const [loading, setLoading] = useState(true);
+function ProfileScreen({ navigation }: any) {
+    const { theme, toggleTheme } = useTheme();
+    const { logout } = useAuth();
+    const [user, setUser] = useState({}); // novo
 
-  useEffect(() => {
-    async function loadUser() {
-      try {
-        console.log('API URL:', apiUrl);
-        console.log('Buscando usuário em:', `${apiUrl}/api/users/1`);
-
-        const response = await fetch(`${apiUrl}/api/users/1`);
-
-        console.log('Status:', response.status, response.statusText);
-
-        if (!response.ok) {
-          const text = await response.text();
-          console.error('Erro body:', text);
-          throw new Error('Erro ao buscar usuário');
+    // novo
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const user = await requestProfileById(1);
+                console.log(user);
+                setUser(user);
+                console.log('Carregou o usuário!');
+            }
+            catch (error) {
+                console.error('Erro ao carregar o perfil do usuário:', error);
+            }
         }
+        fetchProfile();
+    }, []);
 
-        const data = await response.json();
-        setUser(data);
-      } catch (error) {
-        console.error('Erro ao carregar usuário:', error);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    loadUser();
-  }, []);
-
-  if (loading) {
     return (
-      <View
-        style={[styles.container, { backgroundColor: theme.colors.background }]}
-      >
-        <ActivityIndicator size="large" color={theme.colors.primary} />
-        <Text style={{ color: theme.colors.text, marginTop: 10 }}>
-          Carregando perfil...
-        </Text>
-      </View>
-    );
-  }
+        <View style={[styles.container, {backgroundColor: theme.colors.background}]}>
+            <Text style={{ color: theme.colors.text, marginBottom: theme.spacing(1) }}>
+                Profile Screen
+            </Text>
+            <View>
+                <Image source={{ uri: user.image }} style={styles.image}/>
+            </View>
+            <Text style={styles.text}>{user.name}</Text>
+            <Text style={styles.text}>{user.email}</Text>
 
-  if (!user) {
-    return (
-      <View
-        style={[styles.container, { backgroundColor: theme.colors.background }]}
-      >
-        <Text style={{ color: theme.colors.text }}>
-          Erro ao carregar usuário.
-        </Text>
-      </View>
-    );
-  }
-
-  return (
-    <View
-      style={[styles.container, { backgroundColor: theme.colors.background }]}
-    >
-      <Image
-        source={{
-          uri: user.image || 'https://i.pravatar.cc/150?img=5',
-        }}
-        style={styles.avatar}
-      />
-
-      <Text style={[styles.name, { color: theme.colors.text }]}>
-        {user.name}
-      </Text>
-      <Text style={[styles.email, { color: theme.colors.text }]}>
-        {user.email}
-      </Text>
-
-      <View style={{ marginTop: 20 }}>
-        <Button
-          title="Alternar Tema"
-          color={theme.colors.primary}
-          onPress={toggleTheme}
-        />
-        <View style={{ marginTop: 10 }}>
-          <Button
-            title="Ir para Detalhes"
-            onPress={() => navigation.navigate('Details')}
-          />
+            <Button title="Alternar Tema" color={theme.colors.primary} onPress={toggleTheme}/>
+            <Button title="Ir para Detalhes" onPress={ () => navigation.navigate('Details')} />
+            <Button title="Sair" onPress={logout}/>
         </View>
-        <View style={{ marginTop: 10 }}>
-          <Button title="Sair" onPress={logout} />
-        </View>
-      </View>
-    </View>
-  );
+    );
 }
+export default ProfileScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  avatar: { width: 120, height: 120, borderRadius: 60, marginBottom: 20 },
-  name: { fontSize: 22, fontWeight: '600', marginBottom: 8 },
-  email: { fontSize: 16, color: 'gray' },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: { // novo
+        height: 100,
+        width: 100,
+    },
+    text: { fontSize: 14} // novo
 });
