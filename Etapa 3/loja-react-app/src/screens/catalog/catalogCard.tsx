1960222bd245fbf2601c4f3dcc2d458323c22036
gui-lherme1234 <donizetti.g@aluno.ifsp.edu.br>
2025-12-02 09:28:28 -0300

    Uso básico de imagens

--- Etapa 3/loja-react-app/src/screens/catalog/catalogCard.tsx
+++ Etapa 3/loja-react-app/src/screens/catalog/catalogCard.tsx
@@ -1,63 +1,82 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { View, Text, Image, StyleSheet, Button } from 'react-native';
 
-const CatalogCard = ({ product, onBuyPress }: any) => {
-  return (
-    <View style={styles.card}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <View style={styles.details}>
-        <Text style={styles.name}>{product.name}</Text>
-        <Text style={styles.description}>{product.description}</Text>
-        <Text style={styles.price}>${product.price.toFixed(2)}</Text>
-      </View>
-      <View style={styles.buttonsConteiner}>
-        <Button
-          title="Adicionar ao Carrinho"
-          color="#28a745"
-          onPress={onBuyPress}
-        />
-      </View>
-    </View>
-  );
-};
-const styles = StyleSheet.create({
-  card: {
-    backgroundColor: '#fff',
-    borderRadius: 8,
-    padding: 16,
-    marginBottom: 16,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 2,
-  },
-  image: {
-    width: '100%',
-    height: 150,
-    borderRadius: 8,
-    marginBottom: 8,
-  },
-  details: {
-    marginBottom: 8,
-  },
-  name: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 4,
-  },
-  description: {
-    fontSize: 14,
-    color: '#444',
-    marginBottom: 8,
-  },
-  price: {
-    fontSize: 14,
-    color: '#888',
-  },
-  buttonsConteiner: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-});
+import { useShop } from '../../contexts/ShopContext';
+import { useAuth } from '../../contexts/AuthContext';
+
+const CatalogCard = ({product, onBuyPress}: any) => {
+    const { pickImage }= useShop();
+    const { userData } = useAuth();
+
+    return (
+        <View style={styles.card}>
+            <Image  
+                source={{ uri: product.image }}
+                style={styles.image}
+            />
+            <View style={styles.details}>
+                <Text style={styles.name}>{product.name}</Text>
+                <Text style={styles.description}>{product.description}</Text>
+                <Text style={styles.price}>R$ {product.price.toFixed(2)}</Text>
+                <View style={styles.buttonsContainer}>
+                        <Button 
+                            title="Comprar"
+                            color="#28A745"
+                            onPress={onBuyPress}
+                        />
+                        {userData.is_admin ? (
+                                <Button 
+                                    title="Editar"
+                                    color="#007BFF"
+                                    onPress={() => pickImage()}
+                                />
+                            ) : null
+                        }
+                </View>
+            </View>
+        </View>
+    );
+}
 export default CatalogCard;
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        padding: 15,
+        marginBottom: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2},
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+        elevation: 3,
+    },
+    image: {
+        width: '100%',
+        height: 200,
+        borderRadius: 8,
+        borderWidth: 2,
+        borderColor: '#ddd',
+    },
+    details: {
+        paddingHorizontal: 10,
+    },
+    name: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    description: {
+        fontSize: 14,
+        color: '#555',
+    },
+    price: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#28A745',
+        marginVertical: 10,
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    }
+});
